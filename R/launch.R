bb93dc9443d81ccd4c0375862de7955217b5eb99
Michael Braun <braunm@smu.edu>
2021-09-28 22:27:34 -0500

    ready for public display

--- R/launch.R
+++ R/launch.R
@@ -1,22 +1,9 @@
 #' @title Launch app
-#' @description Runs the ABTestDeception app
+#' @description Just run ABTestDeception::launch(). That's it.
 #' @export
-launch <- function(...) {
+launch <- function() {
   ggplot2::theme_set(theme_minimal(base_size=32))
   thematic::thematic_shiny(font='auto')
-
-  ## Compile Sass into css file (this just seems to work better than bslib's functions.)
-##  bs_path <- stringr::str_c(.libPaths(), "/bslib/lib/bs5/scss/") ## but use bslib-provided Bootstrap
-  ## sass::sass(input=sass::sass_file(system.file("inst/app/sass/base.scss", package='ABTestDeception', mustWork=TRUE)),
-  ##            output=system.file("inst/app/www/custom.css", package='ABTestDeception', mustWork=TRUE),
-  ##            options=sass::sass_options(include_path = bs_path),
-  ##            cache=FALSE)
-
- ## sass::sass(input=sass::sass_file("inst/app/sass/base.scss"),
- ##             output="inst/app/www/custom.css",
- ##             options=sass::sass_options(include_path = bs_path),
- ##             cache=FALSE)
-
 
 
 
